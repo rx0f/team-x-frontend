137f345e9141f86eace136c719fd512a31f405d3
Abderaouf Louggani <ja_louggani@esi.dz>
2022-05-13 16:33:49 +0100

    sign up patient

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/shared/layout";
 import Checkout from "./pages/checkout";
 import Login from "./pages/login";
+import SignupPatient from "./pages/signupPatient";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<Layout />}>
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/login" element={<Login/>}/>
+          <Route path="/signup-patient" element={<SignupPatient/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
